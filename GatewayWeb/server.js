18e43feafd420ece2bb7b758f75369ff2257aedc
PV <peter.vezjak@student.um.si>
2024-04-16 16:30:39 +0200

    dodan cors

--- GatewayWeb/server.js
+++ GatewayWeb/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 require('dotenv').config();
 const grpc = require('@grpc/grpc-js');
 const { RentalServiceClient } = require('./generated/rental_grpc_pb');
@@ -14,6 +15,7 @@ const grpcClient = new RentalServiceClient(
 );
 
 app.use(express.json());
+app.use (cors);
 
 app.all('/cars/*', async (req, res) => {
     try {
